10d59fcc6c4d245c61caf07117f09d5fb344106e
colesda <colesda@onid.oregonstate.edu>
2015-01-21 10:03:37 -0800

    01/21/15
    See Change Log -David

--- project_26_SP/TakeMeasurement.swift
+++ project_26_SP/TakeMeasurement.swift
@@ -1,29 +1,32 @@
-//
-//  ViewController.swift
-//  project_26_SP
-//
-//  Created by David Coles on 12/24/14.
-//  Copyright (c) 2014 Remcho Research. All rights reserved.
-//
+/*
+Copyright [2015] [Remcho Research]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import UIKit
 
-class TakeMeasurement: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class TakeMeasurement: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
-    
+ 
     @IBOutlet weak var imageView: UIImageView!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         println("Test Requirement #2: 'Take a Measurement' Loaded")
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+ 
     @IBAction func chooseImageFromPhotoLibrary(sender: AnyObject) {
         let picker = UIImagePickerController()
         picker.delegate = self
